7cd0230fd3c295c09683f96b7d8cae09dce13d28
yusubhin <sdbtnqlss@gmail.com>
2024-08-05 13:10:29 +0900

    [Feat] table selected, hover 스타일 구현 (#32)

--- react_frontend/src/Table.js
+++ react_frontend/src/Table.js
@@ -9,6 +9,7 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Box, Checkbox } from '@mui/material';
 
+// TableCell을 스타일링하는 컴포넌트
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: '#F4F2FF',
@@ -19,18 +20,28 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
   },
 }));
 
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
-    backgroundColor: '#FFFFFF',
-    color: '#25213B'
-  },
-  '&:last-child td, &:last-child th': {
-    border: 0,
-  },
+// TableRow를 스타일링하는 컴포넌트
+const StyledTableRow = styled(TableRow)(({ theme, selected }) => ({
+    '&:last-child td, &:last-child th': {
+        border: 0,
+    },
+    '&:hover': {
+        backgroundColor: '#E4DCFF', // 호버 시 배경색 설정
+    },
+    '&.Mui-selected': {
+        backgroundColor: '#B7AAFF !important', // 선택된 상태에서 배경색 강제 적용
+    },
 }));
 
 export default function CustomizedTables({data, variant = 'default'}) {
     const [selectedRow, setSelectedRow] = React.useState(null);
+
+    const handleRowClick = (index) => {
+        setSelectedRow(index);
+      };
+    console.log(variant);
+    
+
     return (
         <Box sx={{ 
             width: '100%', 
@@ -45,29 +56,42 @@ export default function CustomizedTables({data, variant = 'default'}) {
             <Table sx={{ minWidth: 600 }} aria-label="customized table">
                 <TableHead>
                     <TableRow>
-                        { // 컬럼 제목 설정
-                            variant !== 'default' && <StyledTableCell />    // default가 아니면 컬럼 제목 1칸 비우기
+                        {
+                            // checkbox가 있는 테이블이면 체크박스 셀 추가
+                            variant === 'checkbox' && <StyledTableCell></StyledTableCell> } 
+                        {
+                            // 컬럼 제목 설정
+                            Object.keys(data[0])?.map(col => (<StyledTableCell key={col}>{col}</StyledTableCell>
+                            ))    
                         }
-                        {Object.keys(data[0])?.map(col => <StyledTableCell key={col}>{col}</StyledTableCell>)};
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {
                         // 표에 data 채우기
                         data.map((row, index) => (
-                            <StyledTableRow key={index}>
-                                {variant === 'checkbox' && (
-                                    <StyledTableCell>
-                                        <Checkbox />
-                                    </StyledTableCell>
-                                )}
-                                {Object.values(row).map((value, idx) => (
-                                    <StyledTableCell key={idx} align="left">
-                                        {value}
-                                    </StyledTableCell>
-                                ))}
+                            <StyledTableRow 
+                                key={index}
+                                selected={selectedRow === index}
+                                onClick={() => handleRowClick(index)}
+                            >
+                                {   // checkbox가 있는 테이블이면 체크박스 셀 추가
+                                    variant === 'checkbox' && (
+                                        <StyledTableCell>
+                                            <Checkbox />
+                                        </StyledTableCell>
+                                    )
+                                }
+                                {   // 데이터 값 채우기
+                                    Object.values(row).map((value, idx) => (
+                                        <StyledTableCell key={idx} align="left">
+                                            {value}
+                                        </StyledTableCell>
+                                    ))
+                                }
                             </StyledTableRow>
-                        ))}
+                        ))
+                    }
                 </TableBody>
             </Table>
             </TableContainer>
