6b447861dfa5513191b1d941fff1b3090f1b9620
seojeongHHHui <sjhui0523@gmail.com>
2024-08-06 14:53:26 +0900

    -

--- react_frontend/src/SearchForms.js
+++ react_frontend/src/SearchForms.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as searchFormStyles from './assets/css/searchForm.css';
-import { Dropdown, Form } from 'antd';
+import { Form } from 'antd';
 import DropDown from "./FormItem/DropDown";
 import InputText from "./FormItem/InputText";
 import SelectCalendar from "./FormItem/SelectCalendar";
@@ -58,14 +58,7 @@ export default function SearchForms({ onFormSubmit, formFields }) {
                         defaultSelected={field.defaultSelected}
                     />
                 )
-            })};
-            {/* <SearchAtModal name="searchProject" label="프로젝트명/코드" modalType='프로젝트 찾기' form={form} />
-            <SearchAtModal name="searchLib" label="설비LIB명" modalType='설비LIB 찾기' form={form} />
-            <SearchAtModal name="ModalTest" label="default" required={true} form={form} />
-            <InputText name="email" label="이메일" />
-            <DropDown name="dropDown1" label="드롭다운" options={selectOptions} />
-            <DropDown name="dropDown2" label="필수 드롭다운" required={true} options={selectOptions} defaultSelected={true} />
-            <SelectCalendar name="calendar" label="날짜선택" /> */}
+            })}
             <SearchBtn />
         </Form>
     );
@@ -76,39 +69,6 @@ export function SearchFormPd() {
     return (
         <Form layout="vertical" className={searchFormStyles.form_container}>
             <SearchAtModal name="searchProject2" label="프로젝트코드/명" required={true} />
-            <SearchBtn />
-        </Form>
-    );
-}
-
-export function SearchForm_Ps_1_2({ onFormSubmit }) {
-    const selectYears = [
-        { value: '2024', label: '2024' },
-        { value: '2023', label: '2023' },
-        { value: '2022', label: '2022' },
-        { value: '2021', label: '2021' },
-        { value: '2020', label: '2020' }
-    ];
-    const selectOptions = [
-        { value: 'option1', label: 'Option 1' },
-        { value: 'option2', label: 'Option 2' },
-        { value: 'option3', label: 'Option 3' },
-        { value: 'option4', label: 'Option 4' },
-        { value: 'option5', label: 'Option 5' }
-    ];
-
-    const [form] = Form.useForm();
-
-    const handleFinish = (values) => {
-        onFormSubmit(values);
-    };
-
-    return (
-        <Form form={form} layout="vertical" className={searchFormStyles.form_container} onFinish={handleFinish}>
-            <SearchAtModal name="searchProject" label="프로젝트코드/명" required={true} modalType='프로젝트 찾기' form={form} />
-            <DropDown name="selectYears" label="대상년도" required={true} options={selectYears} defaultSelected={true} />
-            <DropDown name="emissionActivityType" label="배출활동유형" options={selectOptions} />
-
             <SearchBtn />
         </Form>
     );
--- react_frontend/src/components/emperf/Ps_1_2.js
+++ react_frontend/src/components/emperf/Ps_1_2.js
@@ -1,64 +1,64 @@
 import React, { useState } from 'react';
+import SearchForms from "../../SearchForms";
+import { formField_ps12 } from "../../assets/json/searchFormData"
+import InnerTabs from "../../InnerTabs";
 
-import SearchForms from "../../SearchForms.js";
-import {formField_ps12} from "../../assets/json/searchFormData.js"
-
-import InnerTabs from "../../InnerTabs.js";
-import { Tabs, Table } from 'antd';
+import Table from "../../Table.js";
+import project from "../../assets/json/selectedPjt";
 
 export default function Ps_1_2() {
-    {/*
-    const columns = [
-        { title: 'Month', dataIndex: 'month', key: 'month', }, { title: 'Amount', dataIndex: 'amount', key: 'amount', },
-    ];
-    const data = [
-        { key: '1', month: 'Jan', amount: 400, }, { key: '2', month: 'Feb', amount: 300, }, { key: '3', month: 'Mar', amount: 200, }, { key: '4', month: 'Apr', amount: 278, }, { key: '5', month: 'May', amount: 189, }, { key: '6', month: 'Jun', amount: 425, }, { key: '7', month: 'Jul', amount: 453, }, { key: '8', month: 'Aug', amount: 538, },
-    ];
-
-    const columns2 = [
-        { title: 'Month', dataIndex: 'month', key: 'month', }, { title: 'Amount', dataIndex: 'amount', key: 'amount', },
-    ];
-    const data2 = [
-        { key: '6', month: 'Jun', amount: 425, }, { key: '7', month: 'Jul', amount: 453, }, { key: '8', month: 'Aug', amount: 538, }, { key: '9', month: 'Sep', amount: 127, }, { key: '10', month: 'Oct', amount: 543, },
-    ];
-
-    const tabItems = [
-        {
-            label: '사용량',
-            key: '1',
-            children: <Table columns={columns} dataSource={data} />,
-        },
-        {
-            label: '사용금액',
-            key: '2',
-            children: <Table columns={columns2} dataSource={data2} />,
-        },
-    ];
-    */}
     const [formData, setFormData] = useState({});
 
     const handleFormSubmit = (data) => {
         setFormData(data);
     };
 
+    const tabItems = [
+        {
+            label: '사용량',
+            key: '1',
+            children: <Usage formData={formData} />,
+        },
+        {
+            label: '사용금액',
+            key: '2',
+            children: <AmountUsed formData={formData} />,
+        },
+    ];
+
     return (
         <div>
             <h2>실적스코프1,2</h2>
-
-            <SearchForms onFormSubmit={handleFormSubmit} formFields={formField_ps12}/>
-
-            <InnerTabs items={tabItems} />;
-
-            <div>
-                <h3>Form Data:</h3>
-                <ul>
-                    {Object.entries(formData).map(([key, value]) => (
-                        <li key={key}>
-                            <strong>{key}:</strong> {value ? value.toString() : ''}
-                        </li>
-                    ))}
-                </ul>
-            </div>
+            <SearchForms onFormSubmit={handleFormSubmit} formFields={formField_ps12} />
+            <InnerTabs items={tabItems} />
         </div>
     );
+}
+
+
+function Usage({ formData }) {
+    if (!formData || Object.keys(formData).length === 0) {
+        return <p>검색조건을 선택하세요</p>;
+    }
+
+    return (
+        <div>
+            <Table data={project}/>
+        </div>
+    )
+}
+
+function AmountUsed({ formData }) {
+    if (!formData || Object.keys(formData).length === 0) {
+        return <p>검색조건을 선택하세요</p>;
+    }
+
+    const formDataForTable = Object.entries(formData).map(([key, value]) => {
+        return { key: key, value: value != null ? value.toString() : '' };
+    })
+    return (
+        <div>
+            <Table data={formDataForTable}/>
+        </div>
+    )
 }
