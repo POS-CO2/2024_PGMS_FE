f965fbb99e1740eda85ea47d121941efd21b03c5
yusubhin <sdbtnqlss@gmail.com>
2024-09-09 10:26:32 +0900

    [Style] 서치폼 커스텀 스타일로 변경(초록색 아웃라인)

--- react_frontend/src/FormItem/DropDown.js
+++ react_frontend/src/FormItem/DropDown.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { styled } from '@mui/material/styles';
 import * as formItemStyles from '../assets/css/formItem.css';
 import { Form, Select } from 'antd';
 import { CloseOutlined } from '@ant-design/icons';
+
+const CustomSelect = styled(Select)`
+    .ant-select-selector {
+        background-color: transparent !important;
+        border: 2px solid #transparent !important;
+        transition: border-color 0.3s;
+
+        &:hover {
+            border-color: #0EAA00 !important;
+            border-width: 2px !important;
+        }
+
+        &:focus, &:focus-within {
+            outline: none;
+            border-width: 2px !important;
+            border-color: #0EAA00 !important;
+            box-shadow: none !important;
+        }
+    }
+
+    .ant-select-selection-item {
+        &:hover {
+            border-color: #0EAA00 !important;
+        }
+    }
+`;
+
 
 export default function DropDown({ name, label, required=false, options, defaultSelected=false, disabled=false, placeholder="" }) {
     // 옵션 중 가장 긴 옵션의 길이를 계산
@@ -17,7 +45,7 @@ export default function DropDown({ name, label, required=false, options, default
             rules={[{ required: required, message: '${label} 선택은 필수입니다.' }]}
             initialValue={defaultSelected ? options[0].value : undefined}
         >
-            <Select
+            <CustomSelect
                 className={formItemStyles.select_dropdown}
                 style={{ width: calculatedWidth }}
                 allowClear={{ clearIcon: <CloseOutlined style={{color: "red"}} /> }}
@@ -29,7 +57,7 @@ export default function DropDown({ name, label, required=false, options, default
                         {option.label}
                     </Select.Option>
                 ))}
-            </Select>
+            </CustomSelect>
         </Form.Item>
     )
 }
