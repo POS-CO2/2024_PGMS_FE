5eded9b077841e1b69c30d909cd5f7bc22b2c242
seojeongHHHui <sjhui0523@gmail.com>
2024-09-20 17:52:12 +0900

    [Feat] 설비별(ea) 백엔드

--- react_frontend/src/components/analfc/Ea.js
+++ react_frontend/src/components/analfc/Ea.js
@@ -13,8 +13,6 @@ import * as saStyles from "../../assets/css/sa.css"
 import * as psqStyles from "../../assets/css/psq.css"
 import * as XLSX from 'xlsx';
 
-import { libData, typeData, sourceData } from '../../assets/json/saDataEx';
-
 export default function Ea() {
     const [formData, setFormData] = useState(); // 검색 데이터
     const [analEquipData, setAnalEquipData] = useState([]); // 표
@@ -44,23 +42,24 @@ export default function Ea() {
     // 조회 버튼 클릭시 호출될 함수
     const handleFormSubmit = async (data) => {
         setFormData(data);
+        
+        const startDate = `${data.calendar[0].$y}-${(data.calendar[0].$M + 1).toString().padStart(2, '0')}`;
+        const endDate = `${data.calendar[1].$y}-${(data.calendar[1].$M + 1).toString().padStart(2, '0')}`;
+        
+        let url = `/anal/equip?startDate=${startDate}&endDate=${endDate}&selected=${data.selected}`;
+        const response = await axiosInstance.get(url);
+        setAnalEquipData(response.data);
 
         switch (data.selected) {
             case "설비LIB":
-                //let url = `/perf/pjt?pjtId=${data.searchProject.id}&year=${data.actvYear}`;
-                setAnalEquipData(libData);
                 setTableColumn(equipAnalLibColumns);
                 break;
     
             case "설비유형":
-                //let url = `/perf/pjt?pjtId=${data.searchProject.id}&year=${data.actvYear}`;
-                setAnalEquipData(typeData);
                 setTableColumn(equipAnalTypeColumns);
                 break;
     
             case "에너지원":
-                //let url = `/perf/pjt?pjtId=${data.searchProject.id}&year=${data.actvYear}`;
-                setAnalEquipData(sourceData);
                 setTableColumn(equipAnalSourceColumns);
                 break;
     
@@ -68,27 +67,6 @@ export default function Ea() {
                 console.log("알 수 없는 선택입니다.");
                 break;
         }
-
-        /*const response = await axiosInstance.get(url);
-
-        // data가 빈 배열인지 확인
-        if (response.data.length === 0) {
-            // 빈 데이터인 경우, 배열의 필드를 유지하면서 빈 값으로 채운 배열 생성
-            setAnalEquipData([
-                { data: Array(12).fill(null), stack: 'A', label: 'Scope 1' },
-                { data: Array(12).fill(null), stack: 'A', label: 'Scope 2' }
-            ]);
-
-        } else {
-            //차트
-            const scope1Data = response.data.map(perf => perf.scope1 || null);
-            const scope2Data = response.data.map(perf => perf.scope2 || null);
-            const formattedChartPerfs = [
-                { data: scope1Data, stack: 'A', label: 'Scope 1' },
-                { data: scope2Data, stack: 'A', label: 'Scope 2' }
-            ];
-            setAnalEquipData(formattedChartPerfs);
-        }*/
     };
 
     const onDownloadExcelClick = (csvData) => {
