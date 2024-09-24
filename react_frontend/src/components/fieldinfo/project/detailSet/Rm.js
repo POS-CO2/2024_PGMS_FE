6b097d0118253fbba28606d4932fcdcbd3f6be7f
yusubhin <sdbtnqlss@gmail.com>
2024-09-24 10:21:40 +0900

    [Feat] 매출액 관리 메뉴에 차트 추가

--- react_frontend/src/components/fieldinfo/project/detailSet/Rm.js
+++ react_frontend/src/components/fieldinfo/project/detailSet/Rm.js
@@ -4,14 +4,45 @@ import {
     revState, selectedRevState
     } from '../../../../atoms/pdsAtoms';
 import axiosInstance from '../../../../utils/AxiosInstance';
+import ChartCustom from "../../../../ChartCustom";
 import { Card } from '@mui/material';
 import { pjtSalesColumns } from '../../../../assets/json/tableColumn';
 import { TableCustomDoubleClickEdit } from "../../../../TableCustom";
+import { computeMikkTSpaceTangents } from "three/examples/jsm/utils/BufferGeometryUtils.js";
 
 export default function Rm({pjtId}) {
     const [revenues, setRevenues] = useRecoilState(revState);
     const [selectedRev, setSelectedRev] = useRecoilState(selectedRevState);
+    const [chartReves, setChartReves] = useState([]);
     const [year, setYear] = useState(new Date().getFullYear());
+
+    useEffect(() => {
+        handleYearChange(year);
+        transformToRevChart(revenues);
+    }, [])
+
+    const transformToRevChart = (data) => {
+        //data가 빈 배열인지 확인
+        if (data.length === 0) {
+            // 빈 데이터인 경우, 배열의 필드를 유지하면서 빈 값으로 채운 배열 생성
+            setChartReves(
+                { data: Array(12).fill(null), stack: 'A' },
+            );
+
+        } else {
+            // 차트에 사용할 데이터를 숫자 배열로 변환
+            const chartData = data.map(rev => {
+                // 쉼표를 제거하고 숫자로 변환
+                const numericValue = parseFloat(rev.salesAmt.replace(/,/g, '')) || null;
+                return numericValue;
+            });
+
+            const formattedChartReves = [
+                { data: chartData, stack: 'A' },
+            ];
+            setChartReves(formattedChartReves);
+        }
+    }
 
     // 매출액 row 클릭 시 호출될 함수
     const handleRevClick = (rev) => {
@@ -27,11 +58,12 @@ export default function Rm({pjtId}) {
         setYear(year);
         const response = await axiosInstance.get(`/pjt/sales?pjtId=${pjtId}&year=${year}`);
         setRevenues(response.data);
+        transformToRevChart(response.data);
     }
 
     return (
         <>
-            <Card sx={{ width: "50%", height: "auto", borderRadius: "0.5rem" }}>
+            <Card sx={{ width: "30%", height: "auto", borderRadius: "0.5rem" }}>
                 <TableCustomDoubleClickEdit 
                     title='매출액목록' 
                     data={revenues}
@@ -47,8 +79,8 @@ export default function Rm({pjtId}) {
                     year={year}
                 />
             </Card>
-            <Card sx={{ width: "50%", borderRadius: "0.5rem", paddingBottom: "20px" }}>
-                
+            <Card sx={{ width: "70%", borderRadius: "0.5rem" }}>
+                <ChartCustom title={"매출액차트"} data={chartReves} />
             </Card>
         </>
     );
