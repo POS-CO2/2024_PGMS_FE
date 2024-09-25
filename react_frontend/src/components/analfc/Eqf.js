121e27d2f3e844cf526afbfca16b7a2073d783f7
seojeongHHHui <sjhui0523@gmail.com>
2024-09-25 14:09:16 +0900

    [Feat] 배출량 예측 페이지 추가

---
+++ react_frontend/src/components/analfc/Eqf.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import * as mainStyle from '../../assets/css/main.css';
+
+export default function Eqf() { //Emission Quantity Forecast
+
+    return (
+        <div>
+            <div className={mainStyle.breadcrumb}>
+                {"분석및예측 > 배출량 예측"}
+            </div>
+
+        </div>
+    );
+}
