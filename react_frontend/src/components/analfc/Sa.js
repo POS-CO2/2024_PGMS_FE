108b35c7863fc5f04826b42222ca9038cd9ec56c
seojeongHHHui <sjhui0523@gmail.com>
2024-10-04 09:50:02 +0900

    [Feat] 차트 로딩 처리

--- react_frontend/src/components/analfc/Sa.js
+++ react_frontend/src/components/analfc/Sa.js
@@ -5,7 +5,7 @@ import SearchForms from "../../SearchForms";
 import { formField_sa } from "../../assets/json/searchFormData";
 import TableCustom from "../../TableCustom.js";
 import { salesAnalColumns } from '../../assets/json/tableColumn';
-import { Card } from '@mui/material';
+import { Card, CircularProgress } from '@mui/material';
 import { BarChart } from '@mui/x-charts/BarChart';
 import axiosInstance from '../../utils/AxiosInstance';
 import * as mainStyle from '../../assets/css/main.css';
@@ -13,6 +13,20 @@ import * as sysStyles from '../../assets/css/sysmng.css';
 import * as chartStyles from "../../assets/css/chart.css"
 import * as saStyles from "../../assets/css/sa.css"
 import * as XLSX from 'xlsx';
+import styled from 'styled-components';
+
+const Overlay = styled('div')({
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // 반투명 검정색
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 10001, // 스피너가 위에 보이도록 설정
+});
 
 export default function Sa() {
     const [formData, setFormData] = useRecoilState(revAnaSearchForm); // 검색 데이터
@@ -22,6 +36,7 @@ export default function Sa() {
     const [selectedDiv, setSelectedDiv] = useState(null); // 선택된 본부
     const [selectedBar, setSelectedBar] = useState(null); // 선택된 바 상태 추가
     const [highlightedItem, setHighlightedItem] = useState(null); // 강조된 항목 상태 추가
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         // formData값이 있으면(이전 탭의 검색기록이 있으면) 그 값을 불러옴
@@ -32,22 +47,29 @@ export default function Sa() {
 
     // 조회 버튼 클릭시 호출될 함수
     const handleFormSubmit = async (data) => {
+        setIsLoading(true); // 로딩 시작
+
         setFormData(data);
         setHighlightedItem(null); // 강조된 항목 초기화
         
         const startDate = `${data.calendar[0].$y}-${(data.calendar[0].$M + 1).toString().padStart(2, '0')}`;
         const endDate = `${data.calendar[1].$y}-${(data.calendar[1].$M + 1).toString().padStart(2, '0')}`;
 
-        let url = `/anal/sales/table?startDate=${startDate}&endDate=${endDate}`;
-        const tableResponse = await axiosInstance.get(url);
-        setSalesTableData(tableResponse.data);
+        try {
+            let url = `/anal/sales/table?startDate=${startDate}&endDate=${endDate}`;
+            const tableResponse = await axiosInstance.get(url);
+            setSalesTableData(tableResponse.data);
 
-        url = `/anal/sales/div?startDate=${startDate}&endDate=${endDate}`;
-        const perDivChartResponse = await axiosInstance.get(url);
-       setAvgUnitPerDiv(perDivChartResponse.data);
-        url = `/anal/sales/prod?startDate=${startDate}&endDate=${endDate}`;
-        const perProdChartResponse = await axiosInstance.get(url);
-        setUnitPerProd(perProdChartResponse.data);
+            url = `/anal/sales/div?startDate=${startDate}&endDate=${endDate}`;
+            const perDivChartResponse = await axiosInstance.get(url);
+            setAvgUnitPerDiv(perDivChartResponse.data);
+            
+            url = `/anal/sales/prod?startDate=${startDate}&endDate=${endDate}`;
+            const perProdChartResponse = await axiosInstance.get(url);
+            setUnitPerProd(perProdChartResponse.data);
+        } finally {
+            setIsLoading(false); // 로딩 완료
+        }
     };
 
     // 서치폼이 변경될 때 목록 clear
@@ -249,6 +271,11 @@ export default function Sa() {
                     </div>
                 </>
             )}
+            {isLoading && 
+                <Overlay >
+                    <CircularProgress />
+                </Overlay>
+            }
         </div>
     );
 }
