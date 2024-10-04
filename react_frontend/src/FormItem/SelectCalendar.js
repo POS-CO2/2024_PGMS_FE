c233444000ae28fcf8c3f58dc00eda4777a4e56d
yusubhin <sdbtnqlss@gmail.com>
2024-10-04 20:07:54 +0900

    [Style] 조회기간 서치폼 색상, 테두리 두께 변경

--- react_frontend/src/FormItem/SelectCalendar.js
+++ react_frontend/src/FormItem/SelectCalendar.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import * as formItemStyles from '../assets/css/formItem.css';
 import { Form, DatePicker, ConfigProvider } from 'antd';
+import { styled } from '@mui/material/styles';
 import dayjs from 'dayjs';
 
 const { RangePicker } = DatePicker;
+
+const CustomRangePicker = styled(RangePicker)({
+    // 기본 상태의 스타일
+    '&.ant-picker': {
+        borderWidth: '1px',
+        borderColor: '#d9d9d9',
+    },
+    // 호버 상태일 때
+    '&.ant-picker:hover': {
+        outline: 'none',
+        boxShadow: '0 0 0 0.5px #0EAA00 !important',
+        borderColor: '#0EAA00',
+    },
+    // 선택된 상태일 때
+    '&.ant-picker-focused, &.ant-picker-cell-selected, &.ant-picker-cell-range-start, &.ant-picker-cell-range-end': {
+        borderWidth: '2px',
+        borderColor: '#0EAA00',
+    },
+});
 
 export default function SelectCalendar({ name, label, required=false, isAnal=false }) {
 
@@ -37,10 +57,29 @@ export default function SelectCalendar({ name, label, required=false, isAnal=fal
                 label={label}
                 rules={[{ required: required, message: <span style={{ fontFamily: 'SUITE-Regular' }}>{label} 선택은 필수입니다.</span> }]}
             >
-                <RangePicker
-                    picker="month"
-                    cellRender={customMonthRender} // 커스텀 월 렌더링
-                />
+                <ConfigProvider
+                    theme={{
+                        components:{
+                            DatePicker:{
+                                activeBorderColor:"#0EAA00",
+                                cellActiveWithRangeBg : "#c3e59e",
+                                cellRangeBorderColor:"#0EAA00",
+                                hoverBorderColor:"#0EAA00",
+                                multipleItemBg:"#0EAA00",
+                                addonBg:"#0EAA00",
+                                multipleItemBorderColor:"#0EAA00"
+                            },
+                        },
+                        token:{
+                            colorPrimary: "#0EAA00",
+                            fontFamily:"SUITE-Regular",
+                        }
+                }}>
+                    <CustomRangePicker
+                        picker="month"
+                        cellRender={customMonthRender} // 커스텀 월 렌더링
+                    />
+                </ConfigProvider>
             </Form.Item>
         </ConfigProvider>
     );
